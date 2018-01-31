06197c3ca5edb992d68b931584478f60cbbca708
hscong8 <hscong8@yahoo.com>
2018-01-30 17:30:59 -0800

    Verified with GPG

--- application/config/config.php
+++ application/config/config.php
@@ -7,7 +7,7 @@ $config['menu_choices'] = array(
         array('name' => 'Alpha', 'link' => '#'),
         array('name' => 'Bravo', 'link' => '#'),
         array('name' => 'Charlie', 'link' => '/charlie/brown'),
-        array('name' => 'Delta', 'link' => '#'),
+        array('name' => 'Delta', 'link' => ''),
         array('name' => 'Echo', 'link' => '#'),
         array('name' => 'Foxtrot', 'link' => '#'),
         array('name' => 'Golf', 'link' => '#'),
