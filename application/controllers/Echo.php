78430a6a1551a86e494b5e1a1e5085934ef820e6
jade303 <tomjmckay@hotmail.ca>
2018-02-01 18:33:24 -0800

    Commiting local files

---
+++ application/controllers/Echo.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Echo extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+public function index() {
+//	$this->show(1);	
+}
+
+public function Must::wehave() {
+	$this->show(5);	
+}
+
+}
